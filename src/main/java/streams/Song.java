2f56b2ce55dc4d86bbb6c2aa2897188967ea68b1
ht0m <hartht@gmail.com>
2022-01-31 09:41:42 +0100

    Song task

--- src/main/java/streams/Song.java
+++ src/main/java/streams/Song.java
@@ -1,4 +1,50 @@
 package streams;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Song {
+
+    private String title;
+    private int length;
+    private List<String> performers = new ArrayList<>();
+    private LocalDate release;
+
+    public Song(String title, int length, List<String> performers, LocalDate release) {
+        this.title = title;
+        this.length = length;
+        this.performers = performers;
+        this.release = release;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<String> getPerformers() {
+        return performers;
+    }
+
+    public LocalDate getRelease() {
+        return release;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(title, song.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
 }
---
+++ src/main/java/streams/SongService.java
@@ -0,0 +1,41 @@
+package streams;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class SongService {
+
+    private List<Song> songs = new ArrayList<>();
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public Optional<Song> shortestSong() {
+        return songs.stream()
+                .min(Comparator.comparing(Song::getLength));
+    }
+
+    public List<Song> findSongByTitle(String title) {
+        return songs.stream()
+                .filter(song -> song.getTitle().equals(title))
+                .toList();
+    }
+
+    public boolean isPerformerInSong(Song s, String performer) {
+        return songs.stream()
+                .filter(song -> song.equals(s))
+                .anyMatch(song -> song.getPerformers().contains(performer));
+    }
+
+    public List<String> titlesBeforeDate(LocalDate date) {
+        return songs.stream()
+                .filter(song -> song.getRelease().isBefore(date))
+                .map(Song::getTitle)
+                .toList();
+    }
+
+}
